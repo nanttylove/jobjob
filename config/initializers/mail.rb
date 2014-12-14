3c40936bbf26e89b896969629ab9434a6c77b01b
Lattapon Yodsuwan <i@clozed2u.com>
2014-12-14 14:30:05 +0700

    Edit mail settings agian

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
   domain: "gmail.com",
-  user_name: "nanttylove@gmail.com",
-  password: "latlovenant",
+  user_name: "lattapon@zocialinc.com",
+  password: "hellozocial",
   authentication: :plain,
   enable_starttls_auto: true
 }
