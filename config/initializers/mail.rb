ed5ae6a1f73aa78f2efeca0d01c749333fa45de8
Lattapon Yodsuwan <i@clozed2u.com>
2014-12-21 23:12:38 +0700

    Last edit for commit to github

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
   domain: "gmail.com",
-  user_name: "lattapon@zocialinc.com",
-  password: "hellozocial",
+  user_name: "GMAILUSERNAME",
+  password: "GMAILPASSWORD",
   authentication: :plain,
   enable_starttls_auto: true
 }
